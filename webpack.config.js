031ac2298444f9f79b00336507163eaba41c82c3
Erin Bond <erin.bond@code.org>
2020-07-15 11:45:30 -0400

    initial barebones environment setup for ml-playground repo

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const JsConfigWebpackPlugin = require("js-config-webpack-plugin");
+const ScssConfigWebpackPlugin = require("scss-config-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  }
+};
